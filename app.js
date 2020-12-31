4e03f6f03b88b2cdf0d43d7ff36cc2ba13ed3192
ArthurRoseira <arthurroseira95@gmail.com>
2020-12-31 11:15:50 -0300

    WhatsApp API requisition Test

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 
 
 const menuBtn = document.querySelector('#menu-btn')
+const btnWhats = document.querySelector('#whatsIcon')
 
-
+function NewTab(message) {
+ window.open(
+  `https://api.whatsapp.com/send?phone=5541996880868&text=${message}`, "_blank");
+}
 
 
 menuBtn.addEventListener('click', () => {
@@ -10,3 +14,6 @@ menuBtn.addEventListener('click', () => {
  document.body.classList.toggle('open');
 })
 
+btnWhats.addEventListener('click', () => {
+ NewTab(`Olá!!`)
+})
